900a43210709511b85711b8f248eee1ffec0529a
Fortuna <hectorsfortuna@gmail.com>
2024-04-21 16:07:25 -0300

    creation and configuration

---
+++ MauiControleFinanceiro/MauiControleFinanceiro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiControleFinanceiro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
