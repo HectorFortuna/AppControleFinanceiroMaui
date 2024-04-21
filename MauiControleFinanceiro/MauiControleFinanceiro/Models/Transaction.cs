6d5ecab6ddfc9987aa7ee00086773b48055ffbf9
Fortuna <hectorsfortuna@gmail.com>
2024-04-21 18:25:20 -0300

    create repository and add litedb

--- MauiControleFinanceiro/MauiControleFinanceiro/Models/Transaction.cs
+++ MauiControleFinanceiro/MauiControleFinanceiro/Models/Transaction.cs
@@ -8,5 +8,16 @@ namespace MauiControleFinanceiro.Models
 {
     internal class Transaction
     {
+        public int Id { get; set; }
+        public TransactionType Type {  get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public decimal Value { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        Income,
+        Expenses
     }
 }
---
+++ MauiControleFinanceiro/MauiControleFinanceiro/Repositories/TransactionRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace MauiControleFinanceiro.Repositories
+{
+    internal class TransactionRepository : ITransactionRepository
+    {
+        public TransactionRepository() { }
+        public List<Transaction> GetAll() { }
+        public void Add(Transaction transaction) { }
+        public void Update(Transaction transaction) { }
+        public void Delete(Transaction transaction) { }
+    }
+}
